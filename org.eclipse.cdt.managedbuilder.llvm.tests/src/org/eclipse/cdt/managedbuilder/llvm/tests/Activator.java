23481a57f83d0f27eacab27567d4f739dc5416e2
petri.tuononen <petri.tuononen@db86baa9-4c94-7904-9ebe-2ea79a395323>
2011-03-02 06:23:26 +0000

    Fixed compiling errors from tests package.

--- org.eclipse.cdt.managedbuilder.llvm.tests/src/org/eclipse/cdt/managedbuilder/llvm/tests/Activator.java
+++ org.eclipse.cdt.managedbuilder.llvm.tests/src/org/eclipse/cdt/managedbuilder/llvm/tests/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.cdt.managedbuilder.llvm.tests"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = Messages.Activator_0;
 
 	// The shared instance
 	private static Activator plugin;
@@ -24,6 +24,7 @@ public class Activator extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -33,6 +34,7 @@ public class Activator extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
