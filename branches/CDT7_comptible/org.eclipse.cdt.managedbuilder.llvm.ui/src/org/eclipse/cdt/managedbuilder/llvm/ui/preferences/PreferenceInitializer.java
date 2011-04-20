afddf01277b4df6b65edf639078c013a8aacce43
petri.tuononen <petri.tuononen@db86baa9-4c94-7904-9ebe-2ea79a395323>
2011-04-20 17:31:05 +0000

    API compatible with CDT 7.x

---
+++ branches/CDT7_comptible/org.eclipse.cdt.managedbuilder.llvm.ui/src/org/eclipse/cdt/managedbuilder/llvm/ui/preferences/PreferenceInitializer.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Nokia Siemens Networks Oyj, Finland.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *      Nokia Siemens Networks - initial implementation
+ *      Leo Hippelainen - Initial implementation
+ *******************************************************************************/
+package org.eclipse.cdt.managedbuilder.llvm.ui.preferences;
+
+import org.eclipse.cdt.managedbuilder.llvm.ui.LlvmUIPlugin;
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+/**
+ * Class used to initialize the default preference values.
+ * 
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	@Override
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = LlvmUIPlugin.getDefault().getPreferenceStore();
+		store.setDefault(PreferenceConstants.P_LLVM_PATH, ""); //$NON-NLS-1$
+		store.setDefault(PreferenceConstants.P_LLVM_INCLUDE_PATH, ""); //$NON-NLS-1$
+		store.setDefault(PreferenceConstants.P_LLVM_LIBRARY_PATH, ""); //$NON-NLS-1$
+		store.setDefault(PreferenceConstants.P_LLVM_LIBRARIES, ""); //$NON-NLS-1$
+	}
+
+}
