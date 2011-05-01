0004d778936d8e8989e7bd9f2bb1f059ff3561db
petri.tuononen <petri.tuononen@db86baa9-4c94-7904-9ebe-2ea79a395323>
2011-05-01 18:13:27 +0000

    Comments.

--- trunk/org.eclipse.cdt.managedbuilder.llvm.ui/src/org/eclipse/cdt/managedbuilder/llvm/ui/preferences/Messages.java
+++ trunk/org.eclipse.cdt.managedbuilder.llvm.ui/src/org/eclipse/cdt/managedbuilder/llvm/ui/preferences/Messages.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Nokia Siemens Networks Oyj, Finland.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *      Nokia Siemens Networks - initial implementation
+ *      Petri Tuononen - Initial implementation
+ *******************************************************************************/
 package org.eclipse.cdt.managedbuilder.llvm.ui.preferences;
 
 import org.eclipse.osgi.util.NLS;
@@ -21,4 +32,5 @@ public class Messages extends NLS {
 
 	private Messages() {
 	}
+	
 }
